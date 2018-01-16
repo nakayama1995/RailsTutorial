8c2816230d74639d18c215b63ec0de8f13642b10
nakayama <mono649force@yahoo.co.jp>
2018-01-16 11:51:08 +0900

    BASIC認証の追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
   def new
     @article = Article.new
   end
@@ -39,7 +42,7 @@ class ArticlesController < ApplicationController
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-   
+
     redirect_to articles_path
   end
 
